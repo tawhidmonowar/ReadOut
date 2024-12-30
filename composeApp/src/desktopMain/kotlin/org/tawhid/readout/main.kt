59567b452c07f49f77be56be1c98efd60a50c584
Tawhid Monowar <tawhidmonowar@gmail.com>
2024-12-30 06:31:58 +0600

    Configure desktop default window size

--- composeApp/src/desktopMain/kotlin/org/tawhid/readout/main.kt
+++ composeApp/src/desktopMain/kotlin/org/tawhid/readout/main.kt
@@ -1,13 +1,33 @@
 package org.tawhid.readout
 
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
+import org.jetbrains.compose.resources.stringResource
+import org.tawhid.readout.app.App
+import org.tawhid.readout.core.theme.defaultWindowHeight
+import org.tawhid.readout.core.theme.defaultWindowWidth
+import org.tawhid.readout.core.theme.minWindowHeight
+import org.tawhid.readout.core.theme.minWindowWidth
+import readout.composeapp.generated.resources.Res
+import readout.composeapp.generated.resources.app_name
+import java.awt.Dimension
 
 fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "ReadOut",
-    ) {
-        App()
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            state = WindowState(
+                position = WindowPosition(Alignment.Center),
+                size = DpSize(defaultWindowWidth, defaultWindowHeight),
+            ),
+            title = stringResource(Res.string.app_name),
+        ) {
+            window.minimumSize = Dimension(minWindowWidth, minWindowHeight)
+            App()
+        }
     }
 }
