a5e22c2f1f6834db578876f8c0374e9bc4c00a25
Tawhid Monowar <tawhidmonowar@gmail.com>
2025-01-03 13:47:18 +0600

    coil3 dependencies added

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -53,6 +53,7 @@ kotlin {
             implementation(libs.koin.compose.viewmodel)
 
             implementation(libs.bundles.ktor)
+            implementation(libs.bundles.coil)
 
         }
         desktopMain.dependencies {
