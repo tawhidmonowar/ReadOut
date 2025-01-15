8bf1779c2854b79c5f088a4db9bfbb5cbce2a061
tawhidmonowar <tawhidmonowar@gmail.com>
2025-01-15 16:45:06 +0600

    Updated

--- composeApp/src/commonMain/kotlin/org/tawhid/readout/core/utils/ApiKey.kt
+++ composeApp/src/commonMain/kotlin/org/tawhid/readout/core/utils/ApiKey.kt
@@ -1,4 +1,4 @@
 package org.tawhid.readout.core.utils
 
-const val GEMINI_API_KEY = "AIzaSyDbwChT9nKHUxT9t3pEzl_9fFhtGFyFHh0"
-const val CLOUD_TEXT_TO_SPEECH_API_KEY = "AIzaSyAk5RCmKkpllA5mb4-x0HjhiAv7P-4gZ8A"
+const val GEMINI_API_KEY = ""
+const val CLOUD_TEXT_TO_SPEECH_API_KEY = ""
